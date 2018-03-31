1a3cbbf1d15744e788f4af828ce2907f90f3e186
Ramkrishan Patidar <ramkrishan.rails@gmail.com>
2018-03-31 15:04:32 +0530

    book resources routing

--- index.js
+++ index.js
@@ -2,9 +2,34 @@ var express = require('express')
 var app = express();
 
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world 123')
+  res.send('Welcome to express app')
+});
+
+
+// book restfull request routes
+app.get('/books', function (req, res) {
+  res.send('Listing books here..')
+});
+
+app.post('/books', function (req, res) {
+  res.send('I will create book here...')
+});
+
+app.get('/books/:id', function (req, res) {
+  res.send('will show book here')
+});
+
+app.put('/books/:id', function (req, res) {
+  res.send('update book here with put')
+});
+
+app.patch('/books/:id', function (req, res) {
+  res.send('update book here with patch')
+});
+
+app.delete('/books/:id', function (req, res) {
+  res.send('delete book here');
 });
 
 
