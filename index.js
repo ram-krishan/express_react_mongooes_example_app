59ac84c90ff0f5cd8d49567dac182d9018fd8dd1
Ramkrishan Patidar <ramkrishan.rails@gmail.com>
2018-04-07 13:58:24 +0530

    enable logging

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+var paramsLogger =  function (req, res, next) {
+  console.log('========================================');
+  console.log('params', req.params);
+  console.log('query', req.query);
+  console.log('body', req.body);
+  debugger
+  next()
+};
+
+app.use(paramsLogger);
 
 app.get('/', function (req, res) {
-  res.send('Welcome to express app')
+  res.send('<form method="post" action="/books"><input type="text" name="book_name"/><input type="submit">submit</input></form>');
 });
-
 
 // book restfull request routes
 app.get('/books', function (req, res) {
@@ -31,6 +46,5 @@ app.patch('/books/:id', function (req, res) {
 app.delete('/books/:id', function (req, res) {
   res.send('delete book here');
 });
-
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
