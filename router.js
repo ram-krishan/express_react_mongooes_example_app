d2727f269cac57af4507bc2f01f5d433ed288412
Ramkrishan Patidar <ramkrishan.rails@gmail.com>
2018-04-07 18:03:36 +0530

    code optimization

--- router.js
+++ router.js
@@ -1,60 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const _ = require('lodash');
-const User = require('./src/models/user');
+const UsersController = require('./src/controllers/api/users_controller');
 
-
-const dummyUsers = [
-  {
-    id: 1,
-    name: 'ram',
-    age: 20,
-  },
-  {
-    id: 2,
-    name: 'sumit',
-    age: 21,
-  },
-  {
-    id: 3,
-    name: 'vijay',
-    age: 22,
-  },
-  {
-    id: 4,
-    name: 'ragh',
-    age: 23,
-  }
-]
-
-//User restfull request routes
-router.get('/users', function (req, res) {
-  res.send(dummyUsers)
-});
-
-// postman :: body raw  type: JSON/application format.
-router.post('/users', function (req, res) {
-  User.create(req.body).then(user => {
-    res.send({ type: 'post', user: user });
-  }).catch(error => {
-    res.send({type: 'post', error: error });
-  })
-});
-
-router.get('/users/:id', function (req, res) {
-  res.send({ type: 'get', user: _.find(dummyUsers, { id: parseInt(req.params.id, 10) })});
-});
-
-router.put('/users/:id', function (req, res) {
-  res.send({ type: 'put', id: req.query.id })
-});
-
-router.patch('/users/:id', function (req, res) {
-  res.send({ type: 'pathc', id: req.query.id })
-});
-
-router.delete('/users/:id', function (req, res) {
-  res.send({ type: 'delete', id: req.query.id });
-});
+router.get('/users', UsersController.index);
+router.post('/users', UsersController.create);
+router.get('/users/:id', UsersController.show);
+router.put('/users/:id', UsersController.update);
+router.patch('/users/:id', UsersController.update);
+router.delete('/users/:id', UsersController.destroy);
 
 module.exports  = router;
---
+++ src/controllers/api/users_controller.js
@@ -0,0 +1,38 @@
+const User = require('../../models/user');
+
+class UsersController {
+
+  static index (req, res) {
+    User.find().then( users => {
+      res.send(users);
+    });
+  }
+
+  static update(req, res) {
+    User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true }).then(user => {
+      res.send({ type: 'put', user: user });
+    });
+  }
+
+  static create(req, res) {
+    User.create(req.body).then(user => {
+      res.send({ type: 'post', user: user });
+    }).catch(error => {
+      res.send({type: 'post', error: error });
+    });
+  }
+
+  static destroy(req, res) {
+    User.findByIdAndRemove(req.params.id).then(user => {
+      res.send({ type: 'delete', user: user });
+    })
+  }
+
+  static show(req, res) {
+    User.findById(req.params.id).then( user => {
+      res.send({ type: 'get1', user: user });
+    });
+  }
+}
+
+module.exports = UsersController;
