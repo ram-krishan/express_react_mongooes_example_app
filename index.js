6eca270ac11103c1906dc20b726352dea1e2ac21
Ramkrishan Patidar <ramkrishan.rails@gmail.com>
2018-04-28 20:16:13 +0530

    login api implementation with passport

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose  = require('mongoose');
+const passport = require('passport');
+const request = require('request');
+const { Strategy: LocalStrategy } = require('passport-local');
 
+const mongoose  = require('mongoose');
 const router = require('./router');
+const User = require('./src/models/user');
 
 var app = express();
 
@@ -12,6 +16,40 @@ mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+/**
+ * Sign in using Email and Password.
+ */
+passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  User.findOne({ email: email.toLowerCase() }, (err, user) => {
+    if (err) { return done(err); }
+    if (!user) {
+      return done(null, false, { msg: `Email ${email} not found.` });
+    }
+    user.comparePassword(password, (err, isMatch) => {
+      console.log("error", err)
+      console.log("isMatch", isMatch)
+      if (err) { return done(err); }
+      if (isMatch) {
+        return done(null, user);
+      }
+      return done(null, false, { msg: 'Invalid email or password.' });
+    });
+  });
+}));
 
 var paramsLogger =  function (req, res, next) {
   console.log('========================================');
@@ -28,5 +66,11 @@ app.get('/', function (req, res) {
 });
 
 app.use('/api',  router);
+
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    res.send({ user: req.user, message: "you are successfully logged in" });
+  });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const userSchema = new Schema({
+  email:  { type: String, required: [true, 'Email should be present'] },
+  password:  { type: String, required: [true, 'Password should be present'] },
   name:  { type: String, required: [true, 'Name should be present'] },
   age: { type: Number, required: [true, 'Age should be present'] },
 });
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  console.log('candidatePassword', candidatePassword, this)
+
+  if (candidatePassword == this.password) {
+    cb(null, true)
+  } else {
+    cb("Invalid credentials", false);
+  }
+};
 
 var User = mongoose.model('User', userSchema);
 
