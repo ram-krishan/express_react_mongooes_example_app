098a0ac3a17e49d7def21df91686beea65eb9ac1
Ramkrishan Patidar <ramkrishan.rails@gmail.com>
2018-07-16 13:15:14 +0530

    commented passport js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const passport = require('passport');
 const request = require('request');
-const { Strategy: LocalStrategy } = require('passport-local');
+// const { Strategy: LocalStrategy } = require('passport-local');
 
 const mongoose  = require('mongoose');
 const router = require('./router');
@@ -21,36 +21,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
 
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
-});
+// passport.deserializeUser((id, done) => {
+//   User.findById(id, (err, user) => {
+//     done(err, user);
+//   });
+// });
 
 /**
  * Sign in using Email and Password.
- */
-passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-  User.findOne({ email: email.toLowerCase() }, (err, user) => {
-    if (err) { return done(err); }
-    if (!user) {
-      return done(null, false, { msg: `Email ${email} not found.` });
-    }
-    user.comparePassword(password, (err, isMatch) => {
-      console.log("error", err)
-      console.log("isMatch", isMatch)
-      if (err) { return done(err); }
-      if (isMatch) {
-        return done(null, user);
-      }
-      return done(null, false, { msg: 'Invalid email or password.' });
-    });
-  });
-}));
+//  */
+// passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+//   User.findOne({ email: email.toLowerCase() }, (err, user) => {
+//     if (err) { return done(err); }
+//     if (!user) {
+//       return done(null, false, { msg: `Email ${email} not found.` });
+//     }
+//     user.comparePassword(password, (err, isMatch) => {
+//       console.log("error", err)
+//       console.log("isMatch", isMatch)
+//       if (err) { return done(err); }
+//       if (isMatch) {
+//         return done(null, user);
+//       }
+//       return done(null, false, { msg: 'Invalid email or password.' });
+//     });
+//   });
+// }));
 
 var paramsLogger =  function (req, res, next) {
   console.log('========================================');
@@ -68,10 +68,11 @@ app.get('/', function (req, res) {
 
 app.use('/api',  router);
 
-app.post('/login',
-  passport.authenticate('local'),
-  function(req, res) {
-    res.send({ user: req.user, message: "you are successfully logged in" });
-  });
+// app.post('/login',
+//   passport.authenticate('local'),
+//   function(req, res) {
+//     res.send({ user: req.user, message: "you are successfully logged in" });
+//   }
+// );
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
